baa1509108c4ed88ddbe9319ad1a7b14b3b57cac
mohamedromee12 <75943278+mohamedromee12@users.noreply.github.com>
2023-07-10 23:34:21 +0300

    making the first version of calcY which evaluate the equation

--- functionPlotter.py
+++ functionPlotter.py
@@ -1,6 +1,9 @@
 from PySide2.QtWidgets import QApplication, QMainWindow, QLineEdit, QVBoxLayout, QWidget, QPushButton, QHBoxLayout, QLabel
-import sys
 from PySide2.QtGui import QIcon, QFont
+import sys
+import re
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Window(QMainWindow):
@@ -18,7 +21,7 @@ class Window(QMainWindow):
 
         self.blankBlock = QWidget()
         self.blankBlock.setStyleSheet("background-color: #eee;")
-        self.layout.addWidget(self.blankBlock, 90)  
+        self.layout.addWidget(self.blankBlock, 90)
 
         self.horizontalLayout = QHBoxLayout()
         self.layout.addLayout(self.horizontalLayout)
@@ -27,14 +30,12 @@ class Window(QMainWindow):
         self.equation.setFont(QFont("Arial", 13))
         self.horizontalLayout.addWidget(self.equation)
 
-
         self.minWidget = QWidget()
         self.minLayout = QVBoxLayout(self.minWidget)
         self.minLabel = QLabel("Min X:")
         self.minValueEdit = QLineEdit()
         self.minLayout.addWidget(self.minLabel)
         self.minLayout.addWidget(self.minValueEdit)
-
 
         self.maxWidget = QWidget()
         self.maxLayout = QVBoxLayout(self.maxWidget)
@@ -43,18 +44,15 @@ class Window(QMainWindow):
         self.maxLayout.addWidget(self.maxLabel)
         self.maxLayout.addWidget(self.maxValueEdit)
 
-
         self.rangeWidget = QWidget()
         self.rangeLayout = QHBoxLayout(self.rangeWidget)
         self.rangeLayout.addWidget(self.minWidget)
         self.rangeLayout.addWidget(self.maxWidget)
 
-
         self.button = QPushButton("Draw")
         self.button.setStyleSheet(
             "background-color: orange; color: white; font-size: 16px; font-weight: bold; cursor: pointer; border: 1px solid transparent; outline: none; border-radius: 5px; height:30%; width: 20%;")
-        self.button.clicked.connect(self.printValues)
-
+        self.button.clicked.connect(self.plotEquation)
 
         self.rangeWithButtonWidget = QWidget()
         self.rangeWithButtonLayout = QVBoxLayout(self.rangeWithButtonWidget)
@@ -65,9 +63,14 @@ class Window(QMainWindow):
 
         self.setIcon()
 
+
+#################################################################################################
+
     def setIcon(self):
         appIcon = QIcon("Icons\plotter.png")
         self.setWindowIcon(appIcon)
+
+#################################################################################################
 
     def printValues(self):
         minValue = self.minValueEdit.text()
@@ -76,6 +79,45 @@ class Window(QMainWindow):
         print("Max Value:", maxValue)
 
 
+################################################################################################
+
+
+    def calcY(self, expression, numbers):
+        expression = expression.replace(" ", "")
+        pattern = r"([+-]?[0-9]*\.?[0-9]*)\*x\^([0-9]+)"
+        terms = re.findall(pattern, expression)
+        results = []
+        for x in numbers:
+            result = 0
+            for term in terms:
+                print(term)
+                coefficient, exponent = float(term[0]), int(term[1])
+                result += coefficient * (x ** exponent)
+            results.append(result)
+        return results
+
+
+################################################################################################
+
+    def plotEquation(self):
+        minX = int(self.minValueEdit.text())
+        maxX = int(self.maxValueEdit.text())
+
+        print(minX)
+        print(maxX)
+
+        xList = np.linspace(minX, maxX, num=1000)
+        yList = self.calcY(str(self.equation.text()), xList)
+
+        plt.figure(num=0, dpi=120)
+        plt.plot(xList, yList)
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title(str(self.equation.text()))
+        plt.grid(True)
+        plt.show()
+
+##################################################################################################
 
 
 myApp = QApplication(sys.argv)

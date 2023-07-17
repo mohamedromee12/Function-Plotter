1f84be02de985698c65f0bff2b61dceebe7ff2cb
mohamedromee12 <75943278+mohamedromee12@users.noreply.github.com>
2023-07-18 00:28:14 +0300

    update the calcY function to handle y = const and adding more comments in the code

--- function_plotter.py
+++ function_plotter.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import  QMainWindow, QLineEdit, QVBoxLayout, QWidget, QPushButton, QHBoxLayout, QLabel
+from PySide2.QtWidgets import QMainWindow, QLineEdit, QVBoxLayout, QWidget, QPushButton, QHBoxLayout, QLabel
 from PySide2.QtGui import QIcon, QFont
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -13,26 +13,32 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Setting the properties of the main window.
         self.setWindowTitle("Function Plotter")
         self.setGeometry(600, 300, 600, 300)
         self.setMinimumWidth(600)
         self.setMinimumHeight(600)
 
+        # Creating a central widget for the main window and setting it as the
+        # central widget using `setCentralWidget()`. The central widget is the main area of the window
+        # where other widgets and layouts can be added.
         self.centralWidget = QWidget(self)
         self.setCentralWidget(self.centralWidget)
         self.layout = QVBoxLayout(self.centralWidget)
 
+        # Creating a figure, a canvas, and an axis for plotting the graph.
         self.figure = Figure(figsize=(5, 4), dpi=100)
         self.canvas = FigureCanvas(self.figure)
         self.ax = self.figure.add_subplot(111)
         self.ax.set_xlabel('X')
         self.ax.set_ylabel('Y')
-
         self.layout.addWidget(self.canvas, 90)
 
+        # Creating a horizontal layout.
         self.horizontalLayout = QHBoxLayout()
         self.layout.addLayout(self.horizontalLayout)
 
+        # Creating a widget and a vertical layout to hold the equation-related elements.
         self.equationWidget = QWidget()
         self.equationLayout = QVBoxLayout(self.equationWidget)
         self.equation = QLineEdit()
@@ -46,6 +52,7 @@ class Window(QMainWindow):
         self.equationLayout.addWidget(self.error)
         self.horizontalLayout.addWidget(self.equationWidget)
 
+        # Creating a widget and a vertical layout to hold the elements related to the minimum x value.
         self.minWidget = QWidget()
         self.minLayout = QVBoxLayout(self.minWidget)
         self.minLabel = QLabel("Min X:")
@@ -53,6 +60,7 @@ class Window(QMainWindow):
         self.minLayout.addWidget(self.minLabel)
         self.minLayout.addWidget(self.minValueEdit)
 
+        # Creating a widget and a vertical layout to hold the elements related to the maximum x value.
         self.maxWidget = QWidget()
         self.maxLayout = QVBoxLayout(self.maxWidget)
         self.maxLabel = QLabel("Max X:")
@@ -60,23 +68,29 @@ class Window(QMainWindow):
         self.maxLayout.addWidget(self.maxLabel)
         self.maxLayout.addWidget(self.maxValueEdit)
 
+        # Creating a widget and a horizontal layout to hold the elements related to the minimum and maximum x values.
         self.rangeWidget = QWidget()
         self.rangeLayout = QHBoxLayout(self.rangeWidget)
         self.rangeLayout.addWidget(self.minWidget)
         self.rangeLayout.addWidget(self.maxWidget)
 
+        # Creating a QPushButton widget , the `clicked` signal of the button is connected to the `plotEquation` method, which
+        # will be called when the button is clicked.
         self.button = QPushButton("Draw")
         self.button.setStyleSheet(
             "background-color: orange; color: white; font-size: 16px; font-weight: bold; cursor: pointer; border: 1px solid transparent; outline: none; border-radius: 5px; height:30%; width: 20%;")
         self.button.clicked.connect(self.plotEquation)
 
+        # Creating a widget and a vertical layout to hold the elements related to the minimum and maximum
+        # x values and the button. The `rangeWithButtonWidget` is added to the `horizontalLayout`. This arrangement allows the minimum and maximum x value
+        # inputs and the button to be displayed horizontally in the main window.
         self.rangeWithButtonWidget = QWidget()
         self.rangeWithButtonLayout = QVBoxLayout(self.rangeWithButtonWidget)
         self.rangeWithButtonLayout.addWidget(self.rangeWidget)
         self.rangeWithButtonLayout.addWidget(self.button)
-
         self.horizontalLayout.addWidget(self.rangeWithButtonWidget)
 
+        # Calling the function which set the icon of the gui program
         self.setIcon()
 
     #################################################################################################
@@ -97,32 +111,40 @@ class Window(QMainWindow):
         :return: The function does not explicitly return anything.
         """
 
+        # Check if there is any input field.
         if (len(self.equation.text()) == 0 or len(self.minValueEdit.text()) == 0 or len(self.maxValueEdit.text()) == 0):
             self.error.setText("All input fields should be filled")
             self.ax.clear()
             self.canvas.draw()
             return
 
-
+        # Ensure the max and min are numbers.
         if (isNumber(self.minValueEdit.text()) == False or isNumber(self.maxValueEdit.text()) == False):
             self.error.setText("Max x and Min x should be numbers only")
             self.ax.clear()
             self.canvas.draw()
             return
 
-        minX = int(self.minValueEdit.text())
-        maxX = int(self.maxValueEdit.text())
 
-        
+        # Convert max and min to numbers
+        minX = float(self.minValueEdit.text())
+        maxX = float(self.maxValueEdit.text())
+
+
+        # Ensure that max is greater than min
         if (maxX < minX):
             self.error.setText("Max x should be greater than Min x")
             self.ax.clear()
             self.canvas.draw()
             return
 
+
+        # Get x values inbetween the min and max, y values from the expression.
         xList = np.linspace(minX, maxX, num=1000)
         yList = calcY(str(self.equation.text()), xList, self.error)
 
+
+        # Check if there is any error, if there is not error plot the function, else clear the figure.
         if self.error.text() == "":
             self.ax.clear()
             self.ax.plot(xList, yList)
@@ -130,9 +152,5 @@ class Window(QMainWindow):
             self.ax.grid(True)
             self.canvas.draw()
         else:
-            self.ax.clear()    
+            self.ax.clear()
             self.canvas.draw()
-       
-
-
-
--- helper_function.py
+++ helper_function.py
@@ -21,11 +21,11 @@ def calcY(expression, x, error=None):
     with support for basic arithmetic operations and exponentiation.
 
     :param expression: The expression parameter is a string that represents a mathematical expression.
-    It can contain variables, operators, and numbers
-    :param x: The value of x that you want to evaluate the expression at
+    It can contain variables, operators, and numbers.
+    :param x: The value of x that you want to evaluate the expression at.
     :param error: The `error` parameter is a widget or object that is used to display any error messages
     or notifications to the user. It could be a label, text box, or any other UI element that can
-    display text
+    display text.
     :return: The function `calcY` returns the result of evaluating the expression after performing some
     operations and replacements.
     """
@@ -39,13 +39,19 @@ def calcY(expression, x, error=None):
         '-',
     ]
 
+    # Search for any unsupported operators
     for word in re.findall('[a-zA-Z_]+', expression):
         if word not in allowed:
             if error is not None:
-                error.setText(
-                    f"Supported Operators: {', '.join(allowed)}")
+                error.setText(f"Supported Operators: {', '.join(allowed)}")
             return False
 
+    
+    # Handle the case of plotting y = constant
+    expression = re.sub(r'\b(\d+(\.\d*)?|\.\d+)\b', r'\1*x**0', expression)
+
+
+    # Replace ^ with ** for eval function
     toBeReplaced = {
         '^': '**',
     }
